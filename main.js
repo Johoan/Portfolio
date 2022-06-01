a394a56801f273313e08cb6f382349ecce33cb9c
Johoan <38538750+Johoan@users.noreply.github.com>
2022-06-01 14:07:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var a = 30;
+console.log(a);
+
+const name1 = "John";
+const age = 39;
+console.log('My name is ' + name1 + ' and my age is ' + age);
+console.log(`My name is ${name1} and my age is ${age}`);
+let name = prompt('What is your name?');
+console.log(`Your name is ${name}`);
